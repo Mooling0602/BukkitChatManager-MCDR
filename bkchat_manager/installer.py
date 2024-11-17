d8060c988bfe6c426209b78f36b6140a96a67fd9
Mooling0602 <clemooling@outlook.com>
2024-11-17 17:03:29 +0800

    debug

--- bkchat_manager/installer.py
+++ bkchat_manager/installer.py
@@ -12,11 +12,12 @@ def unpack_dependency():
     server_folder = psi.get_mcdr_config()["working_directory"]
     target_folder = f'{server_folder}/plugins'
     if os.path.exists(extra_folder):
+        psi.logger.info("内置依赖项存在")
         for item in os.listdir(extra_folder):
             item_path = os.path.join(extra_folder, item)
             target_path = os.path.join(target_folder, item)
             if not os.path.exists(target_path):
-                shutil.copy(item_path, target_path)
                 psi.logger.info(f"解压依赖项[BukkitAPI插件: {item}] 到 {target_folder} 中...")
+                shutil.copy(item_path, target_path)
             else:
                 psi.logger.warning(f"依赖项[BukkitAPI插件: {item}] 已经存在于目标目录中，请尝试重启服务器以加载插件")
